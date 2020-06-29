138e58b8f88b2b02ffefffc67511b6e54edcbfd4
marynovski <kmarynowski@beautfulcompany.pl>
2020-06-29 15:16:12 +0200

    Fix Main activity/Refactor code

--- app/src/main/java/com/tcenter/tcenter/MainActivity.kt
+++ app/src/main/java/com/tcenter/tcenter/MainActivity.kt
@@ -8,25 +8,25 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+    private fun redirectToLoginActivity() {
+        val intent = Intent(this, LoginActivitty::class.java)
+        startActivity(intent)
     }
 
-    override fun onResume() {
-        super.onResume()
-        /**
-         * 1.    Check if shared preferences file "UserData.xml" exists.
-         * --------------------------------------------------------------
-         * 1.1   If not   -> redirect to login activity
-         * 1.2   If yes   -> check if Boolean IS_AUTHENCTICATED == true
-         * --------------------------------------------------------------
-         * 1.2.1 If true  -> redirect to ticket list activity
-         * 1.2.2 If false -> redirect to login activity
-         */
-        val sharedPreferences = getSharedPreferences("userData", Context.MODE_PRIVATE)
-        val isAuthenticated: Boolean = sharedPreferences.contains("IS_AUTHENTICATED")
+    private fun redicrectToTicketListActivity()
+    {
+        val intent = Intent(this, TicketListActivity::class.java)
+        startActivity(intent)
+    }
+
+    /**
+     * Checks if user is loged in and redirect him to:
+     * Login activity if he's not
+     * Or
+     * Ticket list view if he is
+     */
+    private fun checkIfUserIsAuthenticated(isAuthenticated: Boolean, sharedPreferences: SharedPreferences)
+    {
         if (isAuthenticated) {
             val isLoggedIn: Boolean = sharedPreferences.getBoolean("IS_AUTHENTICATED", false)
             if (isLoggedIn) {
@@ -39,15 +39,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /** REDIRECT TO LOGIN ACTIVITY */
-    private fun redirectToLoginActivity() {
-        val intent = Intent(this, LoginActivitty::class.java)
-        startActivity(intent)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
-    private fun redicrectToTicketListActivity()
-    {
-        val intent = Intent(this, TicketListActivity::class.java)
-        startActivity(intent)
+    override fun onResume() {
+        super.onResume()
+
+        val sharedPreferences = getSharedPreferences("userData", Context.MODE_PRIVATE)
+        val isAuthenticated: Boolean = sharedPreferences.contains("IS_AUTHENTICATED")
+
+        this.checkIfUserIsAuthenticated(isAuthenticated, sharedPreferences)
     }
 }
