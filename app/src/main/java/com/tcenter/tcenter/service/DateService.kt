b5d352a25b1b3d5ec9611a10d7577428631b0f47
marynovski <kmarynowski@beautifulcompany.pl>
2020-07-01 13:42:46 +0200

    Fix reopen ticket 404 error

--- app/src/main/java/com/tcenter/tcenter/service/DateService.kt
+++ app/src/main/java/com/tcenter/tcenter/service/DateService.kt
@@ -46,54 +46,7 @@ class DateService {
 
     fun checkIfDeadlineIsOver(dateTime: JSONObject) : Boolean
     {
-//        2020-06-16T12:59:42.344
-//        val currentDateTime = LocalDateTime.now().toString()
-//        val date_and_time: List<String> = currentDateTime.split("T")
-//        val date: String = date_and_time[0]
-//        val time: String = date_and_time[1]
-//
-//        val year_month_day: List<String> = date.split("-")
-//        val hour_minute_second: List<String> = time.split(":", ".")
-//
-//        val year: Int = year_month_day[0].toInt()
-//        val month: Int = year_month_day[1].toInt()
-//        val day: Int = year_month_day[2].toInt()
-//
-//        val hour: Int = hour_minute_second[0].toInt()
-//        val minute: Int = hour_minute_second[1].toInt()
-//        val second: Int = hour_minute_second[2].toInt()
-//
-//        val dateString: String = dateTime.getString("date")
-//        val date_and_time2: List<String> = dateString.split(" ")
-//        val date2: String = date_and_time2[0]
-//        val time2: String = date_and_time2[1]
-//        val year_month_day2: List<String> = date2.split("-")
-//        val hours_minutes_seconds2: List<String> = time2.split(":")
-//        val year2: Int = year_month_day2[0].toInt()
-//        val monthNumber2: Int = year_month_day[1].toInt()
-//        val day2: Int = year_month_day[2].toInt()
-//        val hour2: Int = hours_minutes_seconds2[0].toInt()
-//        val minute2: Int = hours_minutes_seconds2[1].toInt()
-//        var month2: Int = monthNumber2.toInt()
-//
-//        var isOver = false
-//
-//        println("$year-$month-$day $hour:$minute:$second")
-//        println("$year2-$month2-$day2 $hour2:$minute2")
-//
-//        if (year > year2) {
-//            isOver = true
-//        } else if (month > month2) {
-//            isOver = true
-//        } else if (day > day2) {
-//            isOver = true
-//        } else if (hour > hour2) {
-//            isOver = true
-//        } else if (minute > minute2){
-//            isOver = true
-//        } else {
-//            isOver = false
-//        }
+
 
         return true
     }
